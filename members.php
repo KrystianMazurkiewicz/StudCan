03aaf731546217d8a2d5541d57130f66b565f222
KrystianMazurkiewicz <s354340@oslomet.no>
2022-09-18 14:52:20 +0200

    Adding functionality to search for members by certain attributes

--- members.php
+++ members.php
@@ -11,17 +11,17 @@
   <main>
     <section class="content-container">
       <h1>Member:</h1>
-      <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET">
+      <!-- <form action="?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET"> -->
         <div class="hashtag-options">
           <?php foreach($tags as $tag): ?>
             <label for="<?php echo $tag['name'] ?>" class="hashtag-option">
-              <input type="checkbox" name="<?php echo $tag['name'] ?>" id="<?php echo $tag['name'] ?>">
+              <input type="checkbox" checked name="<?php echo $tag['name'] ?>" id="<?php echo $tag['name'] ?>">
               <?php echo $tag['name'] ?>
             </label>
           <?php endforeach; ?>
         </div>
-        <button id="sort-skills">Search students based on skills</button>
-      </form>
+        <button id="sort-skills" onclick="showMembers()">Search students based on skills</button>
+      <!-- </form> -->
       <p class="available-internships">37 students:</p>
       <section class="list-of-members">
         
@@ -43,24 +43,34 @@
         },';
       endforeach ?>
     ];
+
+    function buildTable(member) {
+      const div = document.createElement('div')
+      div.classList.add('column')
+
+      for (let i = 0; i < Object.keys(membersArrayJS[0]).length; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        row.innerText = member[Object.keys(member)[i]]
+        div.append(row)
+      }
+
+      list_of_members.append(div)
+    }
     
     function showMembers() {
-      membersArrayJS.forEach(member => {
-        const div = document.createElement('div')
-        div.classList.add('column')
-      
-        const first_row = document.createElement('div')
-        first_row.classList.add('row')
-        first_row.innerText = member['username']
-        div.append(first_row)
-        
-        const second_row = document.createElement('div')
-        second_row.classList.add('row')
-        second_row.innerText = member['role']
-        div.append(second_row)
-        
-        list_of_members.append(div)
-      })
+      list_of_members.innerText = ""
+
+      loop1: for (let i = 0; i < membersArrayJS.length; i++) {
+        let checkedBoxes = document.querySelectorAll('input:checked')
+
+        for (let j = 0; j < checkedBoxes.length; j++) {
+          if (checkedBoxes[j].name == membersArrayJS[i]['role'] || i == 0) {
+            buildTable(membersArrayJS[i])
+            continue loop1
+          }
+        }
+      }
     }
     showMembers()
   </script>
